ed0f124e2ca79ead9abc13f18ce783b2a040d705
afouq <afouquet67@gmail.com>
2023-10-09 14:56:56 +0200

    Affichage article // 3ème point

--- techtrendz_dummy/actualite.php
+++ techtrendz_dummy/actualite.php
@@ -5,24 +5,19 @@ require_once __DIR__ . "/lib/article.php";
 require_once __DIR__ . "/templates/header.php";
 
 
-//@todo On doit récupérer l'id en paramètre d'url et appeler la fonction getArticleById récupérer l'article
-
+if ($_GET['id']){
+    $article = getArticleById($pdo,$_GET['id']);
+}
 ?>
-
 
 <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
     <div class="col-10 col-sm-8 col-lg-6">
         <img src="/uploads/articles/3-devops.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
     </div>
     <div class="col-lg-6">
-        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Les meilleurs outils DevOps</h1>
-        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, amet. Cum labore possimus ad vitae minima nesciunt commodi eos.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, amet. Cum labore possimus ad vitae minima nesciunt commodi eos.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, amet. Cum labore possimus ad vitae minima nesciunt commodi eos.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, amet. Cum labore possimus ad vitae minima nesciunt commodi eos.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, amet. Cum labore possimus ad vitae minima nesciunt commodi eos.</p>
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3"><?php echo $article['title'] ?></h1>
+        <p class="lead"><?php echo $article['content']?></p>
     </div>
 </div>
-
 
 <?php require_once __DIR__ . "/templates/footer.php"; ?>
