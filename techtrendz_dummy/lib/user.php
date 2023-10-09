c1c7d77188e866aab3ae1713cfda8f4c8f535cb2
afouq <afouquet67@gmail.com>
2023-10-09 13:53:00 +0200

    Initial commit

---
+++ techtrendz_dummy/lib/user.php
@@ -0,0 +1,37 @@
+<?php
+
+function addUser(PDO $pdo, string $first_name, string $last_name, string $email, string $password, $role = "user")
+{
+    /*
+        @todo faire la requête d'insertion d'utilisateur et retourner $query->execute();
+        Attention faire une requête préparer et à binder les paramètres
+    */
+}
+
+function verifyUserLoginPassword(PDO $pdo, string $email, string $password)
+{
+    $sql= 'Select * from users where email = :email';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':email', $email);
+    $stmt->execute();
+    $user = $stmt->fetch();
+
+    /*
+        @todo faire une requête qui récupère l'utilisateur par email et stocker le résultat dans user
+        Attention faire une requête préparer et à binder les paramètres
+    */
+
+    if(password_verify($password,$user['password'])){
+        return $user;
+    }else{
+        return false;
+    }
+
+    /*
+        @todo Si on a un utilisateur et que le mot de passe correspond (voir fonction  native password_verify)
+              alors on retourne $user
+              sinon on retourne false
+    */
+
+
+}
